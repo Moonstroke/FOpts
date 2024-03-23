e046020e6553a7b2c2808b6c29b3de78d81adde0
Moonstroke <jiema@live.fr>
2024-03-23 16:25:58 +0100

    Add the command-line parser class
    The class defines a first set of methods, two addOption overloads to
    define a nullary or unary command-line option (each overload accepts
    the name of the option and either a nullary or unary callback), and a
    parse method, to run the parser on the provided command-line arguments.
    
    The methods are documented, the class too but incompletely.
    The method bodies are not implemented yet, they only throw an
    exception.

---
+++ src/main/java/io/github/moonstroke/fopts/CLParser.java
@@ -0,0 +1,57 @@
+package io.github.moonstroke.fopts;
+
+import java.util.concurrent.Callable;
+import java.util.function.Consumer;
+
+/**
+ * A simple, fluent, low-boilerplate command-line options parser.
+ *
+ * It implements an event-driven framework, using user-supplied callbacks to
+ * interact with the options encountered.
+ *
+ * @author Moonstroke
+ */
+public class CLParser {
+
+	/**
+	 * Declare an option taking no argument.
+	 *
+	 * @param name     The name of the option
+	 * @param callback The function to call upon encountering the option
+	 *
+	 * @return this object
+	 */
+	public CLParser addOption(String name, Callable<Void> callback) {
+		throw new UnsupportedOperationException("Not implemented yet"); // TODO
+	}
+
+	/**
+	 * Declare an option taking one argument.
+	 *
+	 * @param name     The name of the option
+	 * @param callback The function to call upon encountering the option
+	 *
+	 * @return this object
+	 */
+	public CLParser addOption(String name, Consumer<String> callback) {
+		throw new UnsupportedOperationException("Not implemented yet"); // TODO
+	}
+
+
+	/**
+	 * Parse the given command-line arguments.
+	 *
+	 * @param args The command-line arguments
+	 *
+	 * @return the number of elements parsed (or, equivalently, the index in the
+	 *         array at which the parser stopped processing)
+	 *
+	 * @throws IllegalArgumentException if the arguments cannot be parsed: an
+	 *                                  undeclared option is found or strict parsing
+	 *                                  is enabled and an option is missing an
+	 *                                  argument.
+	 */
+	public int parse(String[] args) throws IllegalArgumentException {
+		throw new UnsupportedOperationException("Not implemented yet"); // TODO
+	}
+}
