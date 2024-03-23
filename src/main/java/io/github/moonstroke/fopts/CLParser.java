34de754872305e2e1bcb3c7ba148b156dca6f290
Moonstroke <jiema@live.fr>
2024-03-23 16:33:08 +0100

    Allow the options to have a long and a short name
    Two overloads of CLParser.addOption are added, each accepting a short
    and a long name for the option in addition of the 0- or 1-arg callback.
    
    Once again, the methods are documented, but not implemented.

--- src/main/java/io/github/moonstroke/fopts/CLParser.java
+++ src/main/java/io/github/moonstroke/fopts/CLParser.java
@@ -26,6 +26,19 @@ public class CLParser {
 	}
 
 	/**
+	 * Declare an option taking no argument.
+	 *
+	 * @param longName  The long name of the option
+	 * @param shortName The short name of the option
+	 * @param callback  The function to call upon encountering the option
+	 *
+	 * @return this object
+	 */
+	public CLParser addOption(String longName, String shortName, Callable<Void> callback) {
+		throw new UnsupportedOperationException("Not implemented yet"); // TODO
+	}
+
+	/**
 	 * Declare an option taking one argument.
 	 *
 	 * @param name     The name of the option
@@ -34,6 +47,30 @@ public class CLParser {
 	 * @return this object
 	 */
 	public CLParser addOption(String name, Consumer<String> callback) {
+		throw new UnsupportedOperationException("Not implemented yet"); // TODO
+	}
+
+	/**
+	 * Declare an option taking one argument.
+	 *
+	 * @param longName  The long name of the option
+	 * @param shortName The short name of the option
+	 * @param callback  The function to call upon encountering the option
+	 *
+	 * @return this object
+	 */
+	public CLParser addOption(String longName, String shortName, Consumer<String> callback) {
+		throw new UnsupportedOperationException("Not implemented yet"); // TODO
+	}
+
+
+	/**
+	 * Specify to raise an error when an argument is missing to an option, instead
+	 * of passing a {@code null} argument value to the callback.
+	 *
+	 * @return this object
+	 */
+	public CLParser strictParsing() {
 		throw new UnsupportedOperationException("Not implemented yet"); // TODO
 	}
 
